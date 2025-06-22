7195925d2ff2099615fb7d48356d7118bbca0db2
akai-keisanki <iago.ad@outlook.com>
2025-06-22 20:10:52 -0300

    added TODO functions to CVG library

--- lib/cvg.cc
+++ lib/cvg.cc
@@ -1,1 +1,93 @@
 #include "cvg.hh"
+
+// generator class
+
+CVG::CVG (void)
+{
+  // TODO
+}
+
+void CVG::add_group (const char&)
+{
+  // TODO
+}
+
+void CVG::remove_group (const char&)
+{
+  // TODO
+}
+
+void CVG::clean_group (const char&)
+{
+  // TODO
+}
+
+void CVG::group_add_character (const char&, const wchar_t&)
+{
+  // TODO
+}
+
+void CVG::group_remove_character (const char&, const wchar_t&)
+{
+  // TODO
+}
+
+void CVG::add_syllable_pattern (const char&, const std::string&)
+{
+  // TODO
+}
+
+void CVG::change_syllable_pattern (const char&, const std::string&)
+{
+  // TODO
+}
+
+void CVG::remove_syllable_pattern (const char&)
+{
+  // TODO
+}
+
+void CVG::add_word_pattern (const char&, const std::string&)
+{
+  // TODO
+}
+
+void CVG::change_word_pattern (const char&, const std::string&)
+{
+  // TODO
+}
+
+void CVG::remove_word_pattern (const char&)
+{
+  // TODO
+}
+
+wchar_t CVG::generate_char (const char&)
+{
+  // TODO
+}
+
+wchar_t CVG::generate_char (void)
+{
+  // TODO
+}
+
+std::wstring CVG::generate_syllable (const char&)
+{
+  // TODO
+}
+
+std::wstring CVG::generate_syllable (void)
+{
+  // TODO
+}
+
+std::wstring CVG::generate_word (const char&)
+{
+  // TODO
+}
+
+std::wstring CVG::generate_word (void)
+{
+  // TODO
+}
